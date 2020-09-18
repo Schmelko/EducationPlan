7c555f1a5cf3dcd02d4d18ee14defbddc5a9c76a
Arnold Barna <arnold.barna@gmail.com>
2020-09-18 13:15:43 +0200

    Add EducationPlanEntry class

---
+++ education_plan_entrty.py
@@ -0,0 +1,6 @@
+class EducationPlanEntry:
+    def __init__(self, teacher_name, subject, class_name, hours_per_week):
+        self.teacher_name = teacher_name
+        self.subject = subject
+        self.class_name = class_name
+        self.hours_per_week = hours_per_week
