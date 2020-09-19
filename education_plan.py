5396b74d9d0c77a62af3aff1b63a52e9df452e1f
Arnold Barna <arnold.barna@gmail.com>
2020-09-19 13:50:18 +0200

    Add EducationPlan class number_of_entries function

--- education_plan.py
+++ education_plan.py
@@ -3,10 +3,13 @@ from education_plan_entry import EducationPlanEntry
 class EducationPlan:
 
     def __init__(self, lines):
-        self.entries = []
+        self._entries = []
         for index in range(0, len(lines), 4):
-            self.entries.append(EducationPlanEntry(
+            self._entries.append(EducationPlanEntry(
                 lines[index].strip('\n'),
                 lines[index+1].strip('\n'),
                 lines[index+2].strip('\n'),
                 lines[index+3].strip('\n')))
+    
+    def number_of_entries(self):
+        return len(self._entries)
